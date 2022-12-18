3e1be10444435dc049536f88805e32ab2ab0991c
ChristofherLucas <lucaschristofher05@gmail.com>
2022-12-18 20:05:39 -0300

    chore: initial setting

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.listen(5000);
+app.listen(3000);
 
 app.get("/", function (req: Request, res: Response) {
   res.json("Hello World!").status(200);
